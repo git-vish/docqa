1ec0aaacc89ffca141b309fe899610522e0c510a
git-vish <cloudmail.vishwajeet@gmail.com>
2024-09-08 14:16:03 +0530

    add chat ui

--- src/main.py
+++ src/main.py
@@ -8,10 +8,12 @@ from langchain.chains.retrieval import create_retrieval_chain
 from document_processor import process_document
 from utils import clear_session_state, init_session_state, update_chat_history
 
-load_dotenv()
-init_session_state()
-
 st.set_page_config(page_title="DocQA | Ask the docs", layout="wide")
+
+with st.spinner("Initializing..."):
+    load_dotenv()
+    init_session_state()
+
 st.title("DocQA")
 
 # Sidebar for document upload
@@ -24,30 +26,39 @@ with st.sidebar:
     if file and not st.session_state.vectorstore:
         with st.spinner("Processing document..."):
             st.session_state.vectorstore = process_document(file)
-        st.success("Document processed successfully!")
 
-        history_aware_retriever = create_history_aware_retriever(
-            llm=st.session_state.llm,
-            retriever=st.session_state.vectorstore.as_retriever(),
-            prompt=st.session_state.rephrase_prompt,
-        )
-        st.session_state.retrieval_chain = create_retrieval_chain(
-            retriever=history_aware_retriever,
-            combine_docs_chain=st.session_state.combine_docs_chain,
-        )
+            history_aware_retriever = create_history_aware_retriever(
+                llm=st.session_state.llm,
+                retriever=st.session_state.vectorstore.as_retriever(),
+                prompt=st.session_state.rephrase_prompt,
+            )
+            st.session_state.retrieval_chain = create_retrieval_chain(
+                retriever=history_aware_retriever,
+                combine_docs_chain=st.session_state.combine_docs_chain,
+            )
+        st.success("Document processed successfully!")
 
 # Main content area
 st.subheader("Ask a Question")
 if st.session_state.vectorstore:
-    question = st.text_input("Enter your question:")
+    # Display the chat history
+    for message in st.session_state.chat_history:
+        with st.chat_message(message.type):
+            st.markdown(message.content)
 
-    if question:
+    if question := st.chat_input("Enter your question:"):
+        with st.chat_message("human"):
+            st.markdown(question)
+
         with st.spinner("Thinking..."):
             output = st.session_state.retrieval_chain.invoke(
                 {"input": question, "chat_history": st.session_state.chat_history}
             )
             answer = output["answer"]
-        st.info(answer)
+
+        with st.chat_message("ai"):
+            st.markdown(answer)
+
         update_chat_history(question, answer)
 else:
     st.write("Upload a document to start asking questions.")
